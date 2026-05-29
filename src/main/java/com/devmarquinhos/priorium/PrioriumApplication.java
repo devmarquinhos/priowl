bf9e459bb4038c4394a54e5b38e3ea0bc881bed6
devmarquinhos <marcos.emanuel.04@gmail.com>
2026-05-29 17:01:44 -0300

    :sparkles: first commit

---
+++ src/main/java/com/devmarquinhos/priorium/PrioriumApplication.java
@@ -0,0 +1,13 @@
+package com.devmarquinhos.priorium;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrioriumApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrioriumApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/devmarquinhos/priorium/PrioriumApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devmarquinhos.priorium;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrioriumApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
