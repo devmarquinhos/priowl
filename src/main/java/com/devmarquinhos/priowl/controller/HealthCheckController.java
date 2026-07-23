b00c609ccf6aaf76ffbf788296cb548ad8a5b75e
devmarquinhos <marcos.emanuel.04@gmail.com>
2026-07-22 22:46:40 -0300

    :ambulance: removes sql query due neon issues

--- src/main/java/com/devmarquinhos/priowl/controller/HealthCheckController.java
+++ src/main/java/com/devmarquinhos/priowl/controller/HealthCheckController.java
@@ -9,19 +9,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/api/health")
 public class HealthCheckController {
-    private final JdbcTemplate jdbcTemplate;
-
-    public HealthCheckController(JdbcTemplate jdbcTemplate) {
-        this.jdbcTemplate = jdbcTemplate;
-    }
-
     @GetMapping
     public ResponseEntity<String> ping() {
-        try {
-            jdbcTemplate.queryForObject("SELECT 1", Integer.class);
-            return ResponseEntity.ok("Priowl API and database is active 🦉");
-        } catch (Exception e) {
-            return ResponseEntity.status(503).body("Priowl API is live, but database is not.");
-        }
+        return ResponseEntity.ok("Priowl API and database is active 🦉");
     }
 }
